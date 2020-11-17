793c4ae5fe0ae93d457bd6cce2d408c2fc12dac6
Jacqueline Johnstone-Gruba <jacqueline.mjg@hotmail.com>
2020-11-17 20:34:14 +0000

    Add product details test

---
+++ spec/features/product_details_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.feature "Visitor clicks on one of the product partials and goes to product page", type: :feature, js: true do
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    @category.products.create!(
+      name:  Faker::Hipster.sentence(3),
+      description: Faker::Hipster.paragraph(4),
+      image: open_asset('apparel1.jpg'),
+      quantity: 10,
+      price: 64.99
+    )
+  end
+
+  scenario "They see all products" do
+    visit root_path
+
+    find('a.btn-default').click
+
+    expect(page).to have_content('Description')
+    
+    save_screenshot
+
+  end
+end
